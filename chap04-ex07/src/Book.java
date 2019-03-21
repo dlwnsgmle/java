aae5256a1e4a5b52364196ab4f2c380778f623d5
USER <USER@AMP-04>
2019-03-21 15:38:43 +0900

    Commit

---
+++ chap04-ex07/src/Book.java
@@ -0,0 +1,16 @@
+
+public class Book {
+
+}
+
+
+
+public class BookArray {
+
+	String titlr, author;
+	public Book(String title, String author) {
+		this.title = title;
+		this.author = author;
+	}
+
+}
